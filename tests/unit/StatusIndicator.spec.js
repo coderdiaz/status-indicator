181b04bfc1486b9062b62be65b8cccf8d986cff1
Javier Diaz <hello@coderdiaz.me>
2019-07-20 19:19:28 -0500

    test: fixed tests

--- tests/unit/StatusIndicator.spec.js
+++ tests/unit/StatusIndicator.spec.js
@@ -8,10 +8,6 @@ const createComponent = propsData => shallowMount(StatusIndicator, { propsData }
 
 describe('VueStatusIndicator.vue', () => {
   let cmp;
-  it('has a mounted hook', () => {
-    expect(typeof StatusIndicator.mounted).toBe('function');
-  });
-
   it('should match the snapshot', () => {
     expect(vm.$el).toMatchSnapshot();
   });
@@ -20,43 +16,55 @@ describe('VueStatusIndicator.vue', () => {
     it('should be showed with the default color if the status prop not set', () => {
       cmp = createComponent();
       expect(cmp.vm.status).toBe('');
-      expect(cmp.vm.$el.active).toBe(undefined);
+      expect(cmp.vm.pulse).toBeFalsy();
+      expect(cmp.vm.$el.hasAttribute('status')).toBeFalsy();
+      expect(cmp.vm.$el.hasAttribute('pulse')).toBeFalsy();
     });
 
-    it('should be showed with the active color if the status prop is set', () => {
+    it('should be showed with the positive color if the status prop is set', () => {
       cmp = createComponent({ status: 'active' });
       expect(cmp.vm.status).toBe('active');
-      expect(cmp.vm.$el.active).toBe('');
+      expect(cmp.vm.pulse).toBeFalsy();
+      expect(cmp.vm.$el.hasAttribute('active')).toBeTruthy();
+      expect(cmp.vm.$el.hasAttribute('pulse')).toBeFalsy();
     });
 
     it('should be showed with the positive color if the status prop is set', () => {
       cmp = createComponent({ status: 'positive' });
       expect(cmp.vm.status).toBe('positive');
-      expect(cmp.vm.$el.positive).toBe('');
+      expect(cmp.vm.pulse).toBeFalsy();
+      expect(cmp.vm.$el.hasAttribute('positive')).toBeTruthy();
+      expect(cmp.vm.$el.hasAttribute('pulse')).toBeFalsy();
     });
 
     it('should be showed with the intermediary color if the status prop is set', () => {
       cmp = createComponent({ status: 'intermediary' });
       expect(cmp.vm.status).toBe('intermediary');
-      expect(cmp.vm.$el.intermediary).toBe('');
+      expect(cmp.vm.pulse).toBeFalsy();
+      expect(cmp.vm.$el.hasAttribute('intermediary')).toBeTruthy();
+      expect(cmp.vm.$el.hasAttribute('pulse')).toBeFalsy();
     });
 
     it('should be showed with the negative color if the status prop is set', () => {
       cmp = createComponent({ status: 'negative' });
       expect(cmp.vm.status).toBe('negative');
-      expect(cmp.vm.$el.negative).toBe('');
+      expect(cmp.vm.pulse).toBeFalsy();
+      expect(cmp.vm.$el.hasAttribute('negative')).toBeTruthy();
+      expect(cmp.vm.$el.hasAttribute('pulse')).toBeFalsy();
     });
 
     it('should be showed without pulse effect when the pulse prop is false by default', () => {
       cmp = createComponent();
-      expect(cmp.vm.status).toBeFalsy();
-      expect(cmp.vm.$el.pulse).toBe(undefined);
+      expect(cmp.vm.status).toBe('');
+      expect(cmp.vm.pulse).toBeFalsy();
+      expect(cmp.vm.$el.hasAttribute('pulse')).toBeFalsy();
     });
 
     it('should be showed with pulse effect when the pulse prop is false by default', () => {
       cmp = createComponent({ pulse: true });
+      expect(cmp.vm.status).toBe('');
       expect(cmp.vm.pulse).toBeTruthy();
-      expect(cmp.vm.$el.pulse).toBe('');
+      expect(cmp.vm.$el.hasAttribute('pulse')).toBeTruthy();
     });
   });
 });
